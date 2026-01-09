f81ab47d505ed99d2c0172ac05edd558c11ff8a0
Tommy Zhou <tommyzhou03@gmail.com>
2026-01-09 12:53:41 +0800

    added some implementation structure for outlook

--- src/outlookContentScript.ts
+++ src/outlookContentScript.ts
@@ -1,1 +1,30 @@
 
+type EmailData = {
+  subject: string;
+  bodyHtml: string;
+  bodyText: string;
+};
+
+function isExtensionContextValid(): boolean {
+  try {
+    return chrome.runtime?.id !== undefined;
+  } catch {
+    return false;
+  }
+}
+
+function getComposeEmailData(): EmailData {
+    // make this more accurate
+    
+    const subjectInput = document.querySelector<HTMLInputElement>(
+        'input[name="subjectbox"]',
+    );
+    const bodyInput = document.querySelector<HTMLTextAreaElement>(
+        'textarea[name="body"]',
+    );
+    return {
+        subject: subjectInput?.value || '',
+        bodyHtml: bodyInput?.value || '',
+        bodyText: bodyInput?.value || '',
+    };
+}
